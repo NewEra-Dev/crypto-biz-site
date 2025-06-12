43e68f5a2104741394e6645b5ba989dd48ccf743
New Era <odunayojohn62@gmail.com>
2025-06-12 17:52:50 +0100

    Re-add fs module to fix ReferenceError

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
+const fs = require('fs'); // Re-added fs module
 const MongoStore = require('connect-mongo');
 const mongoose = require('mongoose');
 require('dotenv').config();
